b074aec5e7454ebcf36f54c6cb94bd84e6c2f560
Gabriel Barros <bielb2@hotmail.com>
2020-08-08 17:29:54 -0300

    update avatar

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const proffys = [
     name: "Gabriel Barros", 
     avatar: "https://avatars1.githubusercontent.com/u/54358297?s=460&u=b1cee0114b18a3c9f904b21d1f972c4960c06bac&v=4", whatsapp: "2345608",
     bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma sdas minhas explosões.",
-    subject: "Química",
+    subject: "Física",
     cost:"20",      
     weekday: [0], 
     time_from: [720],
@@ -25,10 +25,10 @@ const proffys = [
 
     { 
         name: "Mayk Brito", 
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
         whatsapp: "2345608",
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma sdas minhas explosões.",
-        subject: "Química",
+        subject: "Geografia",
         cost:"20",      
         weekday: [1], 
         time_from: [720],
